93e2259f9c07d2b83142c0cb235f63c873bd811c
Heibrancelo <s223529731@deakin.edu.au>
2023-10-10 23:36:23 +1100

    clean up comments

--- routers/router.js
+++ routers/router.js
@@ -3,38 +3,15 @@ let router = express.Router();
 let controller = require('../controllers/controller');
 
 
-router.post('/', function(req, res) {
+router.post("/", function (req, res) {
   // **controller perform db logic
-  controller.postCat(req,res);
+  controller.postCat(req, res);
 
+});
 
-    // let cat = req.body;
-    
-    // cat.imagePath = cat.imagePath || "./images/kitten.jpeg";
-  
-    // console.log(cat);  
-    // insertCat(cat, (err, result) => {
-    //   if (err) {
-    //     return res.json({ statusCode: 500, message: 'Failed to insert cat.' });
-    //   }
-    //   res.json({ statusCode: 201, data: result, message: 'success' });
-    // });
+router.get("/", (req, res) => {
+  // **controller perform db logic
+  controller.getAllCats(req, res);
+});
 
-  });
-  
-  
-  router.get('/', (req, res) => {
-    // **controller perform db logic
-    controller.getAllCats(req,res);
-
-
-    // getAllCats((err, result) => {
-    //   if (err) {
-    //     return res.json({ statusCode: 500, message: 'Failed to fetch cats.' });
-    //   }
-    //   res.json({ statusCode: 200, data: result, message: 'success' });
-    // });
-
-  });
-
-  module.exports = router;
+module.exports = router;
